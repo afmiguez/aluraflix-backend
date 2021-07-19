83bb169c3f6bc75752078b14283216781c6551fb
AF Miguez <afmiguez@gmail.com>
2021-07-19 17:04:37 +0100

    first commit

---
+++ src/test/java/me/afmiguez/alura/alurachallengebackend/AluraChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.afmiguez.alura.alurachallengebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraChallengeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
